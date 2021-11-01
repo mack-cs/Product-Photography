6ba614c6516252341a63f8b1f544d8af191221ed
Mackenzie Shonayi <shonayimack@gmail.com>
2021-11-01 04:09:24 +0200

    I have renamed MainActivity.kt to ProductActivity.kt and its layout to activity_product.xml

--- app/src/main/java/com/mcs/productphotography/ProductActivity.kt
+++ app/src/main/java/com/mcs/productphotography/ProductActivity.kt
@@ -3,9 +3,9 @@ package com.mcs.productphotography
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ProductActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_product)
     }
 }
