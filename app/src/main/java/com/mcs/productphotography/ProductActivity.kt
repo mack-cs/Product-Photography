392a90ced8b0bdb0cb42ece2a79b8abe6bf8496a
Mackenzie Shonayi <shonayimack@gmail.com>
2021-11-19 20:08:51 +0200

    Created UI for the application by adding the following. 1. Bottom Nav bar 2. 2 Fragments one with a form for inputting data and the other one for displaying list added products. Created layouts for both fragments

--- app/src/main/java/com/mcs/productphotography/ProductActivity.kt
+++ app/src/main/java/com/mcs/productphotography/ProductActivity.kt
@@ -2,10 +2,35 @@ package com.mcs.productphotography
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.mcs.productphotography.databinding.ActivityProductBinding
 
 class ProductActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityProductBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_product)
+        binding = ActivityProductBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        val addProductFragment = AddProductFragment()
+        val productsListFragment = ProductListFragment()
+
+        makeCurrentFragment(addProductFragment)
+        binding.bottomNav.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.addProduct -> makeCurrentFragment(addProductFragment)
+                R.id.productList -> makeCurrentFragment(productsListFragment)
+            }
+            true
+        }
+
     }
-}
+    private fun makeCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper, fragment)
+            commit()
+        }
+    }
